a8c7d5b0c6fcdfb9e94b94995d9513e03dcc2ea0
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-07-01 16:35:15 -0500

    Create checkAge.js

---
+++ checkAge.js
@@ -0,0 +1,20 @@
+/*
+Write a function called "checkAge". 
+Given a person's name and age, "checkAge" returns one of two messages:
+"Go home, {insert_name_here}!", if they are younger than 21.
+"Welcome, {insert_name_here}!", if they are 21 or older.
+Naturally, replace "{insert_name_here}" with the given name. :)
+var output = checkAge('Adrian', 22);
+console.log(output); // --> 'Welcome, Adrian!'
+*/
+
+function checkAge(name, age) {
+  let message = ""
+  if (age < 21 ){
+    message = `Go home, ${name}!`;
+  }
+  else {
+    message = `Welcome, ${name}!`;
+  }
+  return message;
+}
