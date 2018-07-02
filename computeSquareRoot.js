1a65211da9ce5d66406925983e3f39dcf6280ea9
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-07-02 01:17:30 -0500

    Create computeSquareRoot.js

---
+++ computeSquareRoot.js
@@ -0,0 +1,19 @@
+
+/*
+Write a function called "computeSquareRoot".
+Given a number, "computeSquareRoot" returns its square root.
+var output = computeSquareRoot(9);
+console.log(output); // --> 3
+
+Do not use Math.sqrt(); for this problem. Instead, use this iterative way of solving the problem:
+
+https://www.deltacollege.edu/dept/basicmath/Babylonian.htm
+*/
+
+function computeSquareRoot(num) {
+  for (let i = 0; i <= num; i++){
+    if( i*i === num){
+      return i
+    }
+  }
+}
