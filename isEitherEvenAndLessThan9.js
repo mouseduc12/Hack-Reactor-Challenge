80b6ad87aa5ffc95ae43540855d28e8119e4e25c
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-07-02 13:35:38 -0500

    Create isEitherEvenAndLessThan9.js

---
+++ isEitherEvenAndLessThan9.js
@@ -0,0 +1,25 @@
+/*
+Write a function called "isEitherEvenAndLessThan9".
+
+Given two numbers, 'isEitherEvenAndLessThan9' returns whether at least one of them is even, and, both of them are less than 9.
+
+var output = isEitherEvenAndLessThan9(2, 4);
+console.log(output); // --> true
+
+var output = isEitherEvenAndLessThan9(72, 2);
+console.log(output); // --> false
+*/
+
+function isEitherEvenAndLessThan9(num1, num2) {
+  if(num1 < 9 && num2 < 9){
+    if(num1 % 2 === 0 || num2 % 2 === 0){
+      return true
+    } else { return false}
+  } else {
+    return false
+  }
+}  
+
+  var output = isEitherEvenAndLessThan9(50, 4);
+  console.log(output); // --> false
+
