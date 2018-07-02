df58cfc10c543eb53226d0bcbb3b623ef3d1334c
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-07-02 09:22:25 -0500

    Create doubleSquareRootOf.js

---
+++ doubleSquareRootOf.js
@@ -0,0 +1,14 @@
+/*
+Write a function called "doubleSquareRootOf".
+Given a number, "doubleSquareRootOf" returns double its square root.
+var output = doubleSquareRootOf(121);
+console.log(output); // --> 22
+*/
+
+function doubleSquareRootOf(num) {
+  for (i = 0; i < num; i++){
+    if(i * i === num){
+      return i * 2;
+    }
+  }
+}
