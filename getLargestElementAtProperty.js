1e1357f9e2c3ae47d99f8d7544be9bdd7d043d71
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-10-22 07:38:14 -0600

    Create getLargestElementAtProperty.js

---
+++ getLargestElementAtProperty.js
@@ -0,0 +1,27 @@
+Write a function called "getLargestElementAtProperty".
+
+Given an object and a key, "getLargestElementAtProperty" returns the largest element in the array located at the given key.
+
+Notes:
+* If the array is empty, it should return undefined.
+* If the property at the given key is not an array, it should return undefined.
+* If there is no property at the key, it should return undefined.
+
+var obj = {
+  key: [1, 2, 4]
+};
+var output = getLargestElementAtProperty(obj, 'key');
+console.log(output); // --> 4
+
+
+function getLargestElementAtProperty(obj, key) {
+  if(!obj[key] || !Array.isArray(obj[key]) || obj[key].length === 0) return undefined;
+  let biggest = obj[key][0]
+  for(let each of obj[key]){
+    if(biggest < each){
+      biggest = each;
+    }
+  }
+ 
+  return biggest;
+  }
