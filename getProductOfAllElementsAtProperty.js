ef500b85d7730b35fef4b8f02d353bb61713f008
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-10-24 07:38:19 -0600

    Create getProductOfAllElementsAtProperty.js

---
+++ getProductOfAllElementsAtProperty.js
@@ -0,0 +1,27 @@
+
+/*
+Write a function called "getProductOfAllElementsAtProperty".
+
+Given an object and a key, "getProductOfAllElementsAtProperty" returns the product of all the elements in the array located at the given key. 
+
+Notes:
+* If the array is empty, it should return 0.
+* If the property at the given key is not an array, it should return 0.
+* If there is no property at the given key, it should return 0.
+
+var obj = {
+  key: [1, 2, 3, 4]
+};
+var output = getProductOfAllElementsAtProperty(obj, 'key');
+console.log(output); // --> 24
+
+function getProductOfAllElementsAtProperty(obj, key) {
+  if(!obj[key] || !Array.isArray(obj[key]) || obj[key].length === 0){
+    return 0
+  }
+  let newArr = obj[key];
+  const output = newArr.reduce((acc, currentValue) => {
+    return acc * currentValue;
+  });
+  return output;
+}*/
