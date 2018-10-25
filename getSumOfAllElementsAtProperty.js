64e907b4ce351dff12cd1de7d6b1f3c73c3a58c6
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-10-25 07:49:16 -0600

    Create getSumOfAllElementsAtProperty.js

---
+++ getSumOfAllElementsAtProperty.js
@@ -0,0 +1,23 @@
+/*
+Write a function called "getSumOfAllElementsAtProperty".
+
+Given an object and a key, "getSumOfAllElementsAtProperty" returns the sum of all the elements in the array located at the given key. 
+
+Notes:
+* If the array is empty, it should return 0.
+* If the property at the given key is not an array, it should return 0.
+* If there is no property at the key, it should return 0.
+
+var obj = {
+  key: [4, 1, 8]
+};
+var output = getSumOfAllElementsAtProperty(obj, 'key');
+console.log(output); // --> 13
+
+
+function getSumOfAllElementsAtProperty(obj, key) {
+  if(!obj[key] || !Array.isArray(obj[key]) || obj[key].length === 0) return 0;
+  let propertyArr = obj[key];
+  const output = propertyArr.reduce((acc, currentValue) => acc + currentValue)
+  return output;
+}*/
