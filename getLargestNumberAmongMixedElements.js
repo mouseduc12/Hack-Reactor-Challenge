ff95a01aeb1fb57e92a797ae30e2335ee14611d5
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-10-26 08:00:07 -0600

    Create getLargestNumberAmongMixedElements.js

---
+++ getLargestNumberAmongMixedElements.js
@@ -0,0 +1,26 @@
+/*
+Write a function called "getLargestNumberAmongMixedElements". 
+
+Given any array, "getLargestNumberAmongMixedElements" returns the largest number in the given array.
+
+Notes:
+* The array might contain values of a type other than numbers.
+* If the array is empty, it should return 0.
+* If the array contains no numbers, it should return 0.
+*/
+var output = getLargestNumberAmongMixedElements([3, 'word', 5, 'up', 3, 1]);
+console.log(output); // --> 5
+
+
+function getLargestNumberAmongMixedElements(arr) { 
+  const output = arr.reduce(function(acc, currentValue){
+    if(typeof currentValue === "number"){
+      if(acc === null || acc < currentValue){
+        acc = currentValue
+      }
+    }
+    return acc;
+  })
+    return output;
+}
+
