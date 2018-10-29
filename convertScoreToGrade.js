0b08b8e8bc0695c8aa4fd0ae5340a398d632ca23
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-10-29 09:08:24 -0600

    Create convertScoreToGrade.js

---
+++ convertScoreToGrade.js
@@ -0,0 +1,15 @@
+function convertScoreToGrade(score) {
+  if(score >= 90 && score <= 100){
+    return "A";
+  } else if(score >= 80 && score <= 89){
+    return "B"
+  } else if(score >= 70 && score <= 79){
+    return "C"
+  } else if(score >= 60 && score <= 69 ){
+    return "D"
+  } else if(score <= 59 && score >=0){
+    return "F"
+  } else {
+    return "INVALID SCORE";
+  }
+}
